41c91ea1ed2a34abaf558a84832f42d9d5594cc3
Gunhot <103306105+Gunhot@users.noreply.github.com>
2023-11-14 21:20:41 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'debug_toolbar',
     "rest_framework",
     "reports",
     "analysts",
